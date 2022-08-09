baba338602692431d0cb15f4f7acd420f3ef1f72
vhivestate <garcia.valeria001@gmail.com>
2022-08-08 19:59:23 -0600

    pages

--- trim-down/src/App.js
+++ trim-down/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import About from "./components/about"
+import Login from "./components/login"
+import Nav from "./components/nav"
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(0)
+
+  function displayPage(){
+    if(currentPage === 0){
+      return <About></About>;
+    }else if(currentPage === 1){
+      return <Login></Login>
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Nav setCurrentPage={setCurrentPage}/>
+      {displayPage()}
+    </>
+  )
 }
 
-export default App;
+export default App
