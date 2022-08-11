f557ffe29e3ee7ac0ed1bf7d39171505368cc49f
lrpineda <luicks212@gmail.com>
2022-08-10 23:04:42 -0600

    added react icons and font styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import { FaFire, FaPoo, FaBeer, FaGithub } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <div>
-   <p>Website by Trim Team    <a href='https://github.com/GerusHays/Trim-Down'> Github Icon </a>
-   </p>
-    </div>
+    <footer className='footer footer-center p-10 bg-base-100'>
+        <div className='grid grid-flow-col gap-4'>
+            <a className='link link-hover'> About Us</a>
+            <a className='link link-hover'> Jobs</a>
+        </div>
+        <div>
+            <div className='grid grid-flow-col gap-4'>
+                <a href='https://github.com/GerusHays/Trim-Down'><FaGithub size="28" /></a>
+            </div>
+        </div>
+    </footer>
   )
 }
 
