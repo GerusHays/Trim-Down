d5e5aa4de23c0525e884e8e212bbb7f1ffdb2fd5
Hays4Days <gerushays@gmail.com>
2022-08-10 19:50:34 -0600

    models done

---
+++ backend/models/Schemas.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {type:String, required:true},
+    fullname: {type:String, required:true},
+    email: {type:String, required:true},
+    entryDate: {type:Date, default:Date.now}
+});
+
+const expenseSchema = new Schema({
+    nicotine: {type:String, required:true},
+    alcohol: {type:String, required:true},
+    recreationalDrugs: {type:String, required:true},
+    streamingServices: {type:String, required:true},
+    eatingOut: {type:String, required:true},
+    user: {type:Schema.Types.ObjectId, ref:'users'},
+});
+
+const Users = mongoose.model('users', userSchema, 'users');
+const Expenses = mongoose.model('expenses', expenseSchema, 'expenses');
+const mySchemas = {'users':Users, 'expenses':Expenses};
+
+module.exports = mySchemas;
