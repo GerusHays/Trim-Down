1e10df99a226c09e8996eb2d6f64aad5055d20a9
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-08-10 19:04:44 -0600

    s omething was updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Login from "./components/login"
 import Dash from './components/dash'
 import Footer from './components/footer'
 import Nav from "./components/nav"
+import './App.css';
+
 
 function App() {
   const [currentPage, setCurrentPage] = useState(0)
