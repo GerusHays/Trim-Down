cd38a5d801fe9be8011a8f4063a251bf2b3ffa19
Hays4Days <gerushays@gmail.com>
2022-08-10 19:36:01 -0600

    index set up but needs backend port

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,17 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/', routesHandler);
+
+// DB Connection
+mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+.then( () => {
+    console.log('DB Connected!');
+})
+.catch( (err) => {
+    console.log(err);
+});
+
+const PORT = process.env.PORT || 4000; // backend routing port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
