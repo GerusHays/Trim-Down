6b748d1a842bb1af637509377eff03edf4c5d847
Kansas Carver <97691873+khcarver1@users.noreply.github.com>
2022-08-15 20:53:35 -0600

    removed left drop down menu

--- src/pages/about.js
+++ src/pages/about.js
@@ -78,4 +78,4 @@ function About() {
   )
 }
 
-export default About
+export default About;
--- src/pages/nav.js
+++ src/pages/nav.js
@@ -1,16 +1,8 @@
 import React from "react";
 import Auth from "../utils/auth";
 
-const navList = ["About", "Register", "Profile", "Login"];
 
 function Nav(props) {
-  const list = navList.map((item) => {
-    return (
-      <li key={item}>
-        <a href={`/${item}`}>{item}</a>
-      </li>
-    );
-  });
 
   const logout = (event) => {
     event.preventDefault();
@@ -21,33 +13,7 @@ function Nav(props) {
     <>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabindex="0" className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16M4 18h7"
-                />
-              </svg>
-            </label>{" "}
-            {/* label */}
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {list}
-            </ul>{" "}
-            {/* List */}
-          </div>{" "}
-          {/* dropdown */}
+          
         </div>{" "}
         {/* navbar-start */}
         <div className="navbar-center">
@@ -73,12 +39,29 @@ function Nav(props) {
               class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a href="/Dashboard" class="justify-between">
+                <a href="profile" class="justify-between">
                   Profile
                 </a>
               </li>
               <li>
                 <a>Settings</a>
+              </li>
+              <li>
+                <a href="about">About</a>
+              </li>
+              <li>
+                <>
+                  <a href="register">
+                    Signup
+                  </a>
+                </>
+              </li>
+              <li>
+                <>
+                  <a href="login">
+                    Login
+                  </a>
+                </>
               </li>
               <li>
                 <>
